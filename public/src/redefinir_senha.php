5bfe13992307010c3e9fdbe5ccfb98a5ef9dc660
renanarida <arida.renan100@gamail.com>
2025-06-27 17:37:47 -0300

    feat: adicionando icon no site

--- public/src/redefinir_senha.php
+++ public/src/redefinir_senha.php
@@ -14,6 +14,7 @@ if ($stmt->rowCount()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/redefinir_senha.css">
+    <link rel="icon" type="image/png" href="../img/padlock.png">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <title>Redefinir Senha</title>
 </head>
--- public/src/solicitar_redefinicao.php
+++ public/src/solicitar_redefinicao.php
@@ -2,17 +2,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/solicitar_redefinicao.css">
+    <link rel="icon" type="image/png" href="../img/padlock.png">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <title>Solicitar Redefinição de Senha</title>
 </head>
 
 <body id="body" class="d-flex justify-content-center align-items-center vh-100">
-    
-    <div id="box-caixa">
-        <form id="solicitar_redefinicao" action="../../enviar_email.php" method="post">
-            <label>Digite seu email:</label>
-            <input type="email" name="email" required>
-            <button type="submit">Redefinir Senha</button>
-        </form>
+    <div id="box-titulo">
+        <h1 class="titulo_aviso">Solicite sua redefinição de Senha:</h1>
+
+        <div id="box-caixa">
+            <form id="solicitar_redefinicao" action="../../enviar_email.php" method="post">
+                <label id="label">Digite seu email:</label>
+                <input type="email" name="email" required>
+                <button type="submit">Redefinir Senha</button>
+            </form>
+        </div>
     </div>
 </body>
